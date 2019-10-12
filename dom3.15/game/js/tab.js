51c5a3fcc72e4288d0e88999e70f341d2c853442
getEleflyingfish <935963409@qq.com>
2019-10-12 18:22:46 +0800

    整合

---
+++ dom3.15/game/js/tab.js
@@ -0,0 +1,25 @@
+﻿var tab = function (id, event) {
+        var oDiv = document.getElementById(id);
+        var oBtn = oDiv.getElementsByTagName("li");
+        var oBox = oDiv.getElementsByTagName("dt");
+        for (var i = 0; i < oBtn.length; i++) {
+                (function (index) {
+                        oBtn[i].addEventListener(event, function () {
+                                for (var i = 0; i < oBtn.length; i++) {
+                                        oBtn[i].className = '';
+                                        oBox[i].className = 'tabSide';
+                                }
+                                this.className = 'active';
+                                oBox[index].className = 'active';
+                        });
+                })(i)
+        }
+}
+window.onload = function () {
+        tab("tabMain1", "mouseover");/*click*/
+        tab("tabMain2", "mouseover");
+        tab("tabMain3", "mouseover");
+}
+
+
+
---
+++ dom3.15/game/js/temp.js
@@ -0,0 +1,4 @@
+﻿
+
+
+export var a = 'jspang';
